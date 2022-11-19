29a060a5d5fcd213790954bcc801da9d3ce6d501
GS9005 <2790939022@qq.com>
2022-11-19 23:07:37 +0800

    Update Service

--- src/main/java/org/xzgtemp/service/BookService.java
+++ src/main/java/org/xzgtemp/service/BookService.java
@@ -1,7 +1,12 @@
 package org.xzgtemp.service;
 
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,40 +26,156 @@ public class BookService {
 	RowMapper<Book> userRowMapper = new BeanPropertyRowMapper<>(Book.class);
 
 
-    public Boolean AddBook(Book book){
-
-
-
-        return false;
+    //----------AddBook------------------------------------------
+    public void AddBook(Book book){
+        //String title, String author, Date publishtime,String publisher,String buyer, Date buytime,
+        //String whereis, Boolean isonshelf,Integer borrowtimes, Boolean orderd, String orderuser
+        KeyHolder holder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO Book(b_id,b_title,b_author,b_buyer,b_buytime,b_whereis,b_isonshelf,b_borrowtimes,b_publishtime,b_publisher) VALUES(?,?,?,?,?,?,?,?,?,?)";
+        if(1 != jdbctemplate.update((conn) -> {
+                    PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                    ps.setObject(1,holder.getKey().longValue());
+                    ps.setObject(2,book.GetTitle());
+                    ps.setObject(3,book.GetAuthor());
+                    ps.setObject(4,book.GetPublishtime());
+                    ps.setObject(5,book.GetPublisher());
+                    ps.setObject(6,book.Getbuyer());
+                    ps.setObject(7,book.Getbuytime());
+                    ps.setObject(8,book.GetWhereis());
+                    ps.setObject(9,book.GetIsonshelf());
+                    ps.setObject(10,book.GetBorrowtimes());
+                    return ps;
+                },
+                holder)
+        ){
+            throw new RuntimeException("Insert failed.");
+        }
     }
 
-
+    //---------GetBookByAttribute------------------------------
     public Book GetBookbyBID(int bid){
-
-        return null;
+        String sql = "SELECT * FROM Book WHERE b_id = ?";
+        return jdbctemplate.queryForObject(sql,
+                (ResultSet rs, int rowNum) -> {
+                    return new Book(
+                            rs.getLong("b_id"),
+                            rs.getString("b_title"),
+                            rs.getString("b_author"),
+                            rs.getDate("b_publishtime"),
+                            rs.getString("b_publisher"),
+                            rs.getString("b_buyer"),
+                            rs.getDate("b_buytime"),
+                            rs.getString("b_whereis"),
+                            rs.getBoolean("b_isonshelf"),
+                            rs.getInt("borrowtimes")
+                    );
+                }
+                ,bid
+        );
     }
 
-    public List<Book> GetBookbyTitleAndAuthor(String s){
-
-        return null;
+    public List<Book> GetBookbyTitleOrAuthor(String s){
+        String sql = "SELECT * FROM Book WHERE b_title LIKE %?% OR b_author LIKE %?%";
+        return jdbctemplate.query(sql,
+                (ResultSet rs, int rowNum) -> {
+                    return new Book(
+                            rs.getLong("b_id"),
+                            rs.getString("b_title"),
+                            rs.getString("b_author"),
+                            rs.getDate("b_publishtime"),
+                            rs.getString("b_publisher"),
+                            rs.getString("b_buyer"),
+                            rs.getDate("b_buytime"),
+                            rs.getString("b_whereis"),
+                            rs.getBoolean("b_isonshelf"),
+                            rs.getInt("borrowtimes")
+                    );
+                },s,s);
     }
+
     public List<Book> GetBookbyTitle(String Title){
-
-        return null;
+        String sql = "SELECT * FROM Book WHERE b_title LIKE %?%";
+        return jdbctemplate.query(sql,
+                (ResultSet rs, int rowNum) -> {
+                    return new Book(
+                            rs.getLong("b_id"),
+                            rs.getString("b_title"),
+                            rs.getString("b_author"),
+                            rs.getDate("b_publishtime"),
+                            rs.getString("b_publisher"),
+                            rs.getString("b_buyer"),
+                            rs.getDate("b_buytime"),
+                            rs.getString("b_whereis"),
+                            rs.getBoolean("b_isonshelf"),
+                            rs.getInt("borrowtimes")
+                    );
+                },Title);
     }
+
     public List<Book> GetBookbyAuthor(String Author){
-
-        return null;
+        String sql = "SELECT * FROM Book WHERE b_author LIKE %?%";
+        return jdbctemplate.query(sql,
+                (ResultSet rs, int rowNum) -> {
+                    return new Book(
+                            rs.getLong("b_id"),
+                            rs.getString("b_title"),
+                            rs.getString("b_author"),
+                            rs.getDate("b_publishtime"),
+                            rs.getString("b_publisher"),
+                            rs.getString("b_buyer"),
+                            rs.getDate("b_buytime"),
+                            rs.getString("b_whereis"),
+                            rs.getBoolean("b_isonshelf"),
+                            rs.getInt("borrowtimes")
+                    );
+                },Author);
     }
-
 
     public List<Book> GetALlBooks(){
         return jdbctemplate.query("SELECT * FROM Book",new BeanPropertyRowMapper<>(Book.class));
     }
 
+    //----------ChangBookAttribute------------------------------
+    //need to pass arguments
     public void ChangeBookTitle(Book book){
-
+        String sql = "UPDATA Book SET b_title = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
     }
-    //还有其他的"ChangeBook"+其他属性名 函数
 
+    public void ChangeBookAuthor(Book book){
+        String sql = "UPDATA Book SET b_author = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
+    }
+
+    public void ChangeBookBuyer(Book book){
+        String sql = "UPDATA Book SET b_buyer = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
+    }
+
+    public void ChangeBookBuytime(Book book){
+        String sql = "UPDATA Book SET b_buytime = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
+    }
+
+    public void ChangeBookWhereis(Book book){
+        String sql = "UPDATA Book SET b_whereis = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
+    }
+
+    public void ChangeBookIsonshelf(Book book){
+        String sql = "UPDATA Book SET b_isonshelf = ? WHERE b_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Book no found by id");
+        }
+    }
 }
--- src/main/java/org/xzgtemp/service/DocumentService.java
+++ src/main/java/org/xzgtemp/service/DocumentService.java
@@ -1,8 +1,13 @@
 package org.xzgtemp.service;
 
 
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,27 +27,82 @@ public class DocumentService {
 
 
 
-    public Boolean AddDocument(Document document){
-
-
-        
-        return false;
+    //---------------AddDocunment---------------------------------
+    public void AddDocument(Document document){
+        //String title, String author, String uploaduser, Date uploadtime, String filepath
+        KeyHolder holder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO Document(d_id,d_title,d_author,d_uploader,d_uploadtime,d_filepath,d_downloadtimes) VALUES(?,?,?,?,?,?,?)";
+        if(1 != jdbctemplate.update((conn) -> {
+                    PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                    ps.setObject(1,holder.getKey().longValue());
+                    ps.setObject(2,document.GetTitle());
+                    ps.setObject(3,document.GetAuthor());
+                    ps.setObject(4,document.GetUploader());
+                    ps.setObject(5,document.GetUploadtime());
+                    ps.setObject(6,document.GetFilepath());
+                    ps.setObject(7,document.GetDownloadtimes());
+                    return ps;
+                },
+                holder)
+        ){
+            throw new RuntimeException("Insert failed.");
+        }
     }
 
-    public Document GetDocumentsbyBID(int bid){
-
-        return null;
+    //------------GetDocunmentByAttribute-----------------------------
+    public Document GetDocumentsbyDID(int did){
+        String sql = "SELECT * FROM Document WHERE d_id = ?";
+        return jdbctemplate.queryForObject(sql,
+                (ResultSet rs, int rowNum) -> {
+                    return new Document(
+                            rs.getLong("d_id"),
+                            rs.getString("d_title"),
+                            rs.getString("d_author"),
+                            rs.getString("d_uploader"),
+                            rs.getDate("d_uploadtime"),
+                            rs.getString("d_filepath"),
+                            rs.getInt("d_downloadtimes")
+                    );
+                },
+                did
+        );
     }
 
     public List<Document> GetDocumentsbyTitle(String Title){
-
-        return null;
+        String sql = "SELECT * FROM Document WHERE d_title LIKE %?%";
+        return jdbctemplate.query(sql,
+                (ResultSet rs,int rowNum) -> {
+                    return new Document(
+                            rs.getLong("d_id"),
+                            rs.getString("d_title"),
+                            rs.getString("d_author"),
+                            rs.getString("d_uploader"),
+                            rs.getDate("d_uploadtime"),
+                            rs.getString("d_filepath"),
+                            rs.getInt("d_downloadtimes")
+                    );
+                },
+                Title
+        );
     }
 
 
     public List<Document> GetDocumentsbyAuthor(String Author){
-
-        return null;
+        String sql = "SELECT * FROM Document WHERE d_author = ?";
+        return jdbctemplate.query(sql,
+                (ResultSet rs,int rowNum) -> {
+                    return new Document(
+                            rs.getLong("d_id"),
+                            rs.getString("d_title"),
+                            rs.getString("d_author"),
+                            rs.getString("d_uploader"),
+                            rs.getDate("d_uploadtime"),
+                            rs.getString("d_filepath"),
+                            rs.getInt("d_downloadtimes")
+                    );
+                },
+                Author
+        );
     }
 
 
@@ -50,10 +110,40 @@ public class DocumentService {
         return jdbctemplate.query("SELECT * FROM Document",new BeanPropertyRowMapper<>(Document.class));
     }
 
-
+    //----------ChangDocumentAttribute------------------------------
+    //need pass arguments
     public void ChangeDocumentTitle(Document document){
-
+        String sql = "UPDATA Document SET d_title = ? WHERE d_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Document no found by id.");
+        }
     }
-    //还有其他的"ChangeDocument"+其他属性名 函数
 
+    public void ChangeDocumentAuthor(Document document){
+        String sql = "UPDATA Document SET d_author = ? WHERE d_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Document no found by id.");
+        }
+    }
+
+    public void ChangeDocumentuploader(Document document){
+        String sql = "UPDATA Document SET d_uploader = ? WHERE d_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Document no found by id.");
+        }
+    }
+
+    public void ChangeDocumentfilepath(Document document){
+        String sql = "UPDATA Document SET d_filepath = ? WHERE d_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Document no found by id.");
+        }
+    }
+
+    public void ChangeDocumentdownloadtimes(Document document){
+        String sql = "UPDATA Document SET d_downloadtimes = ? WHERE d_id = ? ";
+        if(1 != jdbctemplate.update(sql)){
+            throw new RuntimeException("Document no found by id.");
+        }
+    }
 }
