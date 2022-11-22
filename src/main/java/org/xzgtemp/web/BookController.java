717db6e3d51ff417ac4090b071981196a3ffec29
xusiqi <2935331819@qq.com>
2022-11-22 13:18:46 +0800

    book表格点击跳转

---
+++ src/main/java/org/xzgtemp/web/BookController.java
@@ -0,0 +1,49 @@
+package org.xzgtemp.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.xzgtemp.entity.User;
+import org.xzgtemp.service.BookService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+
+@RequestMapping("/books")
+@Controller
+public class BookController{
+    
+	@Autowired
+	BookService bookservice;
+
+	public static final String KEY_USER = "__user__";
+
+
+    
+	@GetMapping("/{bid}")
+	public ModelAndView showBook(@PathVariable("bid") Long bid,HttpSession session) {
+		if (session.getAttribute(KEY_USER) ==null){
+			return new ModelAndView("redirect:/signin.html");
+		}
+		Map<String, Object> model = new HashMap<>();
+        model.put("user",(User)session.getAttribute(KEY_USER));
+        model.put("book",bookservice.GetBookbyBID(bid));
+		return new ModelAndView("bookdetial.html",model);
+	}
+    
+
+    @GetMapping("/{bid}/{cid}")
+    public ModelAndView showCopy(HttpSession session){
+		if (session.getAttribute(KEY_USER) ==null){
+			return new ModelAndView("redirect:/signin.html");
+		}
+        return new ModelAndView("redirect:/search.html");
+    }
+
+}
