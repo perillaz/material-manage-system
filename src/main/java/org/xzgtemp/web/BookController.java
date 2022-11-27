f9df8d25fb53ce6897bb70e0746f020475b90017
zhang-shuyi-wuhan <zhangshuyixiannv@outlook.com>
2022-11-27 22:39:09 +0800

    BookController部分修改

--- src/main/java/org/xzgtemp/web/BookController.java
+++ src/main/java/org/xzgtemp/web/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.xzgtemp.entity.Book;
 import org.xzgtemp.entity.User;
 import org.xzgtemp.service.BookService;
+import org.xzgtemp.service.CopyService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +25,8 @@ public class BookController{
     
 	@Autowired
 	BookService bookservice;
+	@Autowired
+	CopyService copyservice;
 
 	public static final String KEY_USER = "__user__";
 
@@ -37,12 +40,80 @@ public class BookController{
 		Map<String, Object> model = new HashMap<>();
         model.put("user",(User)session.getAttribute(KEY_USER));
         model.put("book",bookservice.GetBookbyBID(bid));
-		//TODO：显示单册
-		//TODO：购买者id
+        model.put("copys", copyservice.GetCopybyBID(bid));
 		return new ModelAndView("bookdetial.html",model);
 	}
 
 	//TODO：预约，借阅
+	@PostMapping("/borrow")
+	public ModelAndView doBorrow(@RequestParam("cid") Long cid, @RequestParam("user") User user,HttpSession session) {
+		try{
+			if (session.getAttribute(KEY_USER) ==null){
+				return new ModelAndView("redirect:/signin.html");
+			}
+			Map<String, Object> model = new HashMap<>();
+			if(copyservice.GetCopybyID(cid).getCanbeborrow()) {
+				if(copyservice.GetCopybyID(cid).getCanbereserve()) {
+					copyservice.doBorrowCopy(cid, user);
+					model.put("borrowsucceed", "borrow succeeds!");
+				}
+				else if(user.getId()==copyservice.GetCopybyID(cid).getReserver()) {
+					copyservice.doBorrowCopy(cid, user);
+					model.put("borrowsucceed", "borrow succeeds!");
+				}
+				else {
+					model.put("borrowfailed", "borrow fails!");
+				}
+			}
+			return new ModelAndView("redirect:/bookdetial.html",model);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ModelAndView("redirect:/error/unknown");
+		}
+	}
+	
+	@PostMapping("/reserve")
+	public ModelAndView doReserve(@RequestParam("cid") Long cid, @RequestParam("user") User user, HttpSession session) {
+		try{
+			if (session.getAttribute(KEY_USER) ==null){
+				return new ModelAndView("redirect:/signin.html");
+			}
+			Map<String, Object> model = new HashMap<>();
+			if (copyservice.GetCopybyID(cid).getCanbereserve()) {
+				copyservice.ReserveCopy(cid, user);
+				model.put("reservesucceed","reserve succeeds!");
+			}
+			
+			return new ModelAndView("redirect:/bookdetial.html",model);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ModelAndView("redirect:/error/unknown");
+		}
+	}
+	
+	@PostMapping("/return")
+	public ModelAndView doreturn(@RequestParam("cid") Long cid, @RequestParam("bid") Long bid, HttpSession session) {
+		try{
+			if (session.getAttribute(KEY_USER) ==null){
+				return new ModelAndView("redirect:/signin.html");
+			}
+			copyservice.ReturnCopy(cid, bid);
+			return new ModelAndView("redirect:/bookdetial.html");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ModelAndView("redirect:/error/unknown");
+		}
+	}
+	/*@GetMapping("/borrow")
+	public ModelAndView borrowcopy(@PathVariable("cid") Long cid,HttpSession session) {
+		if (session.getAttribute(KEY_USER)==null) {
+			return new ModelAndView("redirect:/signin.html");
+		}
+		
+	}*/
 
 	@GetMapping("/change")
 	public ModelAndView changeBookInfo(
