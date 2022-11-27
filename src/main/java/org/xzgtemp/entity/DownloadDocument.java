dcf5910cb17d63259b0c9d9ee174fe1a2cc9582b
xusiqi <2935331819@qq.com>
2022-11-27 13:40:12 +0000

    误删恢复
    Signed-off-by: xusiqi <2935331819@qq.com>

---
+++ src/main/java/org/xzgtemp/entity/DownloadDocument.java
@@ -0,0 +1,70 @@
+package org.xzgtemp.entity;
+
+import java.sql.Date;
+
+public class DownloadDocument {
+	private Long id;
+	private String uid;
+	private Long did;
+	private String dtitle;
+	private Date downloadtime;
+	
+	public DownloadDocument(){
+		
+	}
+
+	public DownloadDocument(Long id, String uid, Long did, String dtitle, Date downloadtime) {
+		this.id = id;
+		this.uid = uid;
+		this.did = did;
+		this.dtitle = dtitle;
+		this.downloadtime = downloadtime;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id){
+		this.id = id;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid){
+		this.uid = uid;
+	}
+
+	public Long getDid() {
+		return did;
+	}
+
+	public void setDid(Long did){
+		this.did = did;
+	}
+
+	public String getDtitle() {
+		return dtitle;
+	}
+
+
+	public void setDtitle(String dtitle) {
+		this.dtitle = dtitle;
+	}
+
+	public Date getDownloadtime(){
+		return downloadtime;
+	}
+
+	public void setDownloadtimeCur(){
+
+//TODO: 将downloadtime设置为当前时间
+
+	}
+	public void setDownloadtime(Date downloadtime){
+		this.downloadtime = downloadtime;
+	}
+    
+}
